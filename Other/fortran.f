afe7be49d8ac07ce95b28562768cfde55f6c52e2
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-10-27 11:34:41 +0530

    Fotran

--- Other/fortran.f
+++ Other/fortran.f
@@ -2,28 +2,29 @@
       PROGRAM FIZZBUZZ
 	  INTEGER i , x
 	  DO i = 1 , 100
-    IF(MOD(i,15) . EQ . 0) THEN
-      WRITE(*,*) "FizzBuzz"
-    ELSE IF(MOD(i,3) . EQ . 0) THEN
-      WRITE(*,*) "Fizz"
-    ELSE IF(MOD(i,5) . EQ . 0) THEN
-      WRITE(*,*) "Buzz"
-    ELSE
-      WRITE(*,*) i
-    ENDIF
-    ENDDO
-
+	  IF(MOD(i,15) . EQ . 0) THEN
+		  WRITE(*,*) "FizzBuzz"
+	  ELSE IF(MOD(i,3) . EQ . 0) THEN
+		  WRITE(*,*) "Fizz"
+	  ELSE IF(MOD(i,5) . EQ . 0) THEN
+		  WRITE(*,*) "Buzz"
+	  ELSE
+	    WRITE(*,*) i
+	  ENDIF
+10    CONTINUE
+      ENDDO
 	  DO i = 1 , 100
-    WRITE(*,*) "Enter a Number and Press Enter!"
-    read(*,*),x
-    IF(MOD(x,15) . EQ . 0) THEN
-      WRITE(*,*) "FizzBuzz"
-    ELSE IF(MOD(x,3) . EQ . 0) THEN
-      WRITE(*,*) "Fizz"
-    ELSE IF(MOD(x,5) . EQ . 0) THEN
-      WRITE(*,*) "Buzz"
-    ELSE
-      WRITE(*,*) i
-    ENDIF
-    ENDDO
+	    WRITE(*,*) "Enter a Number and Press Enter!"
+	  read(*,*),x
+	  IF(MOD(x,15) . EQ . 0) THEN
+		  WRITE(*,*) "FizzBuzz"
+	  ELSE IF(MOD(x,3) . EQ . 0) THEN
+		  WRITE(*,*) "Fizz"
+	  ELSE IF(MOD(x,5) . EQ . 0) THEN
+		  WRITE(*,*) "Buzz"
+	  ELSE
+	    WRITE(*,*) i
+	  ENDIF
+25    CONTINUE
+      ENDDO
       END
