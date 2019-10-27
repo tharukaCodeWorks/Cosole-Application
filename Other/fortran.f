8a2b0a5c1940f5a6f183f9c471994260e1832632
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-10-27 11:06:21 +0530

    Fotran

--- Other/fortran.f
+++ Other/fortran.f
@@ -1,19 +1,20 @@
 
-      PROGRAM FIZZBUZZ
+PROGRAM FIZZBUZZ
+
 	  INTEGER i , x
 	  DO i = 1 , 100
-	  IF(MOD(i,15) . EQ . 0) THEN
-		WRITE(*,*) "FizzBuzz"
-	  ELSE IF(MOD(i,3) . EQ . 0) THEN
-		WRITE(*,*) "Fizz"
-	  ELSE IF(MOD(i,5) . EQ . 0) THEN
-		WRITE(*,*) "Buzz"
-	  ELSE
-	    WRITE(*,*) i
-	  ENDIF
-10    CONTINUE
-      ENDDO
-c	  WRITE(*,*) "TESTING INITIALIZING..."	  
+      IF(MOD(i,15) . EQ . 0) THEN
+        WRITE(*,*) "FizzBuzz"
+      ELSE IF(MOD(i,3) . EQ . 0) THEN
+        WRITE(*,*) "Fizz"
+      ELSE IF(MOD(i,5) . EQ . 0) THEN
+        WRITE(*,*) "Buzz"
+      ELSE
+        WRITE(*,*) i
+      ENDIF
+    ENDDO
+
+    WRITE(*,*) "Note: If you want to stop please press CTRL + Z"
 	  DO i = 1 , 100
 	  WRITE(*,*) "Enter a Number and Press Enter!"
 	  read(*,*),x
@@ -28,4 +29,4 @@ c	  WRITE(*,*) "TESTING INITIALIZING..."
 	  ENDIF
 25    CONTINUE
       ENDDO
-      END	  	
+      END
