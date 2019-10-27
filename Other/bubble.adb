57b209fbf21ce3e3c78a58927b451a58401b3d79
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-10-27 13:36:59 +0530

    Ada

--- Other/bubble.adb
+++ Other/bubble.adb
@@ -1,30 +1,31 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Bubble is
-    Data : array(1..10) of Integer;
+    numArray : array(1..10) of Integer;
     Temp : Integer;
 begin
     Put_Line("Please input the series 10 of numbers");
 
-    for I in 1..Data'Length loop
-        Put("Number: ");
-        Data(I) := Integer'Value(Get_Line);
+    for I in 1..numArray'Length loop
+        Put("Number " & Integer'Image(I) & ": ");
+        numArray(I) := Integer'Value(Get_Line);
     end loop;
 
-    for I in reverse Data'Range loop
-        for J in Data'First .. I loop
-          if Data(I) < Data(J) then
-            Temp := Data(J);
-            Data(J) := Data(I);
-            Data(I) := Temp;
+    for I in reverse numArray'Range loop
+        for J in numArray'First .. I loop
+          if numArray(I) < numArray(J) then
+            Temp := numArray(J);
+            numArray(J) := numArray(I);
+            numArray(I) := Temp;
           end if;
         end loop;
     end loop;
 
-    Put_Line("------ SORTED ARRAY ------");
-    for I in 1..Data'Length loop
-      Put(Integer'Image(Data(I)));
+    Put_Line("------ Sorting Array ------");
+    for I in 1..numArray'Length loop
+      Put(Integer'Image(numArray(I)));
       New_Line(1);
     end loop;
+    Put_Line("-------- Completed --------");
 
 end Bubble;
