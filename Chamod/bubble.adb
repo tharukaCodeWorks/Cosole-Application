f6ca05ff0cb3afdc815b406f6b23f19da0f8074c
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-10-27 12:47:25 +0530

    Ada

--- Chamod/bubble.adb
+++ Chamod/bubble.adb
@@ -1,10 +1,8 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 
-procedure Bubble is
+procedure Hello is
     type MY_ARRAY is array(1..10) of Integer;
     Data : MY_ARRAY;
-    Temp : Integer;
 begin
     Put_Line("Please input the series 10 of numbers");
 
@@ -14,7 +12,7 @@ begin
     end loop;
 
     for I in reverse MY_ARRAY'Range loop
-
+        Put("Number: ");
     end loop;
 
-end Bubble;
+end Hello;
