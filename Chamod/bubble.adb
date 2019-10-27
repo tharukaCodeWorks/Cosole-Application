aca29b10d1cd5de44225c75b241f86e2cedaa640
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-10-27 12:01:53 +0530

    Ada

---
+++ Chamod/bubble.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure Main is
+    type MY_ARRAY is array(1..9) of Integer;
+    Data : MY_ARRAY;
+
+begin
+    Put_Line("Please input the series of numbers");
+
+    for I in 1..MY_ARRAY'Length loop
+        Data(I) := Integer'Value(Get_Line);
+    end loop;
+end Main;
