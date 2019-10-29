f8a76fc4bae03332cff026e6438839dd884f6ac7
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-10-29 12:45:54 +0530

    Perl

--- Other/perl.pl
+++ Other/perl.pl
@@ -1,9 +1,21 @@
-#!/usr/bin/perl -w
+use strict;
+use File::Find;
 
-opendir(DIR, "/");
-@files = grep(/\.conf$/,readdir(DIR));
-closedir(DIR);
+my @dirs  = qw( / );
+my $exten = '.conf';
+my @files = _get_files(\@dirs, $exten);
 
+sub _get_files{
+    my $dirs = shift;
+    my $what = shift;
+    my @files;
+    my $want = sub {
+        -f && /\Q$exten\E$/ && push @files, $File::Find::name
+    };
+    find($want, @{$dirs});
+    @files
+}
+my $file;
 foreach $file (@files) {
    print "$file\n";
 }
