a28f467eda69446eb2cbb87777f0e7e9785dcc7f
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-11-01 03:32:39 +0530

    prolog

--- Chamod/prlog.pl
+++ Chamod/prlog.pl
@@ -1,13 +1,14 @@
-#!/usr/bin/gprolog --consult-file
-
-:- initialization(main).
+:- 
+ initialization(main).
 
 fizzbuzz(N,'FizzBuzz') :- N mod 3 =:= 0, N mod 5 =:= 0.
 fizzbuzz(N,'Fizz') :- N mod 3 =:= 0, N mod 5 =\= 0.
 fizzbuzz(N,'Buzz') :- N mod 3 =\= 0, N mod 5 =:= 0.
 fizzbuzz(N,N) :- N mod 3 =\= 0, N mod 5 =\= 0.
 
-writeln(X) :- write(X) ,nl.
+writeln(X) :- 
+  write(X) ,nl.
+  
 unit_test :-
   write('please enter number to test:'),
   read(X), nl,
@@ -16,7 +17,7 @@ unit_test :-
 
 main :-
     bagof(Num, between(1,1000,Num), Numlist),
-    maplist(fizzbuzz,Numlist,FBList),
-    maplist(writeln,FBList),
-    unit_test(),
+    maplist(fizzbuzz,Numlist,FBList), nl,
+    maplist(writeln,FBList), nl,
+    unit_test,
     halt.
